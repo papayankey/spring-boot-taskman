8684b09ee0d76559b692f377ec92470a1f53161a
papayankey <yankeybenneth@gmail.com>
2021-12-01 17:02:37 +0000

    initial commit

---
+++ src/main/java/io/papayankey/taskman/TaskmanApplication.java
@@ -0,0 +1,13 @@
+package io.papayankey.taskman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmanApplication.class, args);
+	}
+
+}

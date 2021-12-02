3fb655a16cfbbdeb04a64126ca791806c5760f68
papayankey <yankeybenneth@gmail.com>
2021-12-02 06:37:31 +0000

    added TaskDto

---
+++ src/main/java/io/papayankey/taskman/task/TaskDto.java
@@ -0,0 +1,24 @@
+package io.papayankey.taskman.task;
+
+public class TaskDto {
+
+    private String description;
+
+    private Boolean completed;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(Boolean completed) {
+        this.completed = completed;
+    }
+}

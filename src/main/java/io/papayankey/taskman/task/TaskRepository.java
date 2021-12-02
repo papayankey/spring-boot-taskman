7f4fe021a082f421ff1119e3675182c30c2a68f7
papayankey <yankeybenneth@gmail.com>
2021-12-02 06:39:39 +0000

    added TaskRepository

---
+++ src/main/java/io/papayankey/taskman/task/TaskRepository.java
@@ -0,0 +1,13 @@
+package io.papayankey.taskman.task;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Integer> {
+
+    List<Task> findByCompleted(Boolean completed);
+
+}

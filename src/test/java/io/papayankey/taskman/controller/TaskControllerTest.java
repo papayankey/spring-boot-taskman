498af318b510dbbf773b06848311801e584916ad
papayankey <yankeybenneth@gmail.com>
2022-04-21 08:44:00 +0000

    added sonarqube and jacoco for code quality and coverage metrics

--- src/test/java/io/papayankey/taskman/controller/TaskControllerTest.java
+++ src/test/java/io/papayankey/taskman/controller/TaskControllerTest.java
@@ -14,6 +14,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.util.UriBuilder;
 
 import java.util.List;
 
